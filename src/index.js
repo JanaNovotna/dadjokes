99ffcfb38372b99591debc8c3e0db8ef7bee967b
Jana Novotná <jana.posedelova@gmail.com>
2020-06-14 17:22:38 +0200

    Snad hotový úkol č. 15

--- src/index.js
+++ src/index.js
@@ -1,44 +1,8 @@
+import Vue from 'vue';
+import App from './App.vue';
 import './index.html';
-import './style.css';
-
-
-let upLikes = 0;
-let downLikes = 0;
-
-// const btnUp = document.querySelector('#btn-up');
-// const btnDown = document.querySelector('#btn-down');
-
-// btnUp.addEventListener('click', () => {
-//   upLikes += 1;
-//   const likesUpElm = document.querySelector('#likes-up');
-//   likesUpElm.textContent = upLikes;
-// });
-
-// btnDown.addEventListener('click', () => {
-//   downLikes += 1;
-//   const likesDownElm = document.querySelector('#likes-down');
-//   likesDownElm.textContent = downLikes;
-// });
 
 new Vue({
   el: '#app',
-
-  data: {
-
-  },
-
-  methods: {
-    btnUp() {
-      upLikes += 1;
-      const likesUpElm = document.querySelector('#likes-up');
-      likesUpElm.textContent = upLikes;
-    },
-    btnDown() {
-      downLikes -= 1;
-      const likesDownElm = document.querySelector('#likes-down');
-      likesDownElm.textContent = downLikes;
-    }
-  }
-
-
+  render: h => h(App),  // nevíme co to dělá, ale funguje to
 });
