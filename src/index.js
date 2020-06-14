32138d4f45fae99c8e33c72837a03f32daebdf44
Jana Novotná <jana.posedelova@gmail.com>
2020-06-14 16:00:23 +0200

    úkol 14 funguje, nevím jak to, ale funguje. Jupíjájé! :)

--- src/index.js
+++ src/index.js
@@ -1,21 +1,44 @@
 import './index.html';
 import './style.css';
 
+
 let upLikes = 0;
 let downLikes = 0;
 
-const btnUp = document.querySelector('#btn-up');
-const btnDown = document.querySelector('#btn-down');
+// const btnUp = document.querySelector('#btn-up');
+// const btnDown = document.querySelector('#btn-down');
 
-btnUp.addEventListener('click', () => {
-  upLikes += 1;
-  const likesUpElm = document.querySelector('#likes-up');
-  likesUpElm.textContent = upLikes;
+// btnUp.addEventListener('click', () => {
+//   upLikes += 1;
+//   const likesUpElm = document.querySelector('#likes-up');
+//   likesUpElm.textContent = upLikes;
+// });
+
+// btnDown.addEventListener('click', () => {
+//   downLikes += 1;
+//   const likesDownElm = document.querySelector('#likes-down');
+//   likesDownElm.textContent = downLikes;
+// });
+
+new Vue({
+  el: '#app',
+
+  data: {
+
+  },
+
+  methods: {
+    btnUp() {
+      upLikes += 1;
+      const likesUpElm = document.querySelector('#likes-up');
+      likesUpElm.textContent = upLikes;
+    },
+    btnDown() {
+      downLikes -= 1;
+      const likesDownElm = document.querySelector('#likes-down');
+      likesDownElm.textContent = downLikes;
+    }
+  }
+
+
 });
-
-btnDown.addEventListener('click', () => {
-  downLikes += 1;
-  const likesDownElm = document.querySelector('#likes-down');
-  likesDownElm.textContent = downLikes;
-});
-
